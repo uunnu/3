203b4d239d3353052920523c444740458c6392c6
Sodoo <scarlett2037@gmail.com>
2022-10-10 16:36:42 +0800

    label

--- react-weather-app/src/api.js
+++ react-weather-app/src/api.js
@@ -1,14 +1,10 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Key': '8019f60d1fmsh15439831767bb8dp17ec79jsn3ac74dd037eb',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
-fetch('/cities', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
--- react-weather-app/src/components/search/search.js
+++ react-weather-app/src/components/search/search.js
@@ -1,20 +1,36 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from '../../api';
 
-const Search = ({onSearchChange}) => {
-   
+const Search = ({ onSearchChange }) => {
+
     const [search, setSearch] = useState(null);
 
     const loadOptions = (inputValue) => {
-        
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, 
+            geoApiOptions
+        )
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: ,
+                            label: ,
+                        }
+                    })
+                }
+            })
+            .catch(err => console.error(err));
     }
-    
+
     const handleOnChange = (searchData) => {
         setSearch(searchData);
         onSearchChange(searchData);
     }
-   
-    return(
+
+    return (
         <AsyncPaginate
             placeholder="Search for city"
             debounceTimeout={600}
