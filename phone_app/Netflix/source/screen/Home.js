705ea15a74adc0e76beb3d3753f4a94464fb8e6e
uunnu <sw21d017@mandakh.edu.mn>
2023-04-18 12:54:43 +0800

    begging

---
+++ phone_app/Netflix/source/screen/Home.js
@@ -0,0 +1,15 @@
+import { View, Text, SafeAreaView, StyleSheet, ScrollView, Image, Pressable } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+    return (
+        <SafeAreaView style={styles.container}>fdsf</SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#ccc",
+    }
+});
