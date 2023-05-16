75d2f9d5e59bf947883cc4ff2e133b07cf589023
uunnu <sw21d017@mandakh.edu.mn>
2023-05-16 16:59:42 +0800

    img1

--- phone_app/Netflix/source/screen/Home.js
+++ phone_app/Netflix/source/screen/Home.js
@@ -1,16 +1,24 @@
 import { View, Text, SafeAreaView, StyleSheet, ScrollView, Image, Pressable } from 'react-native'
 import React from 'react'
+import { MaterialCommunityIcons, Feather } from '@expo/vector-icons'
 
 export default function Home() {
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.view1}>
-                <Text style={styles.txt1}>For User</Text>
-                <View>
-
-                </View>
+        <View style={styles.view1}>
+            <View>
+                <Text style={styles.txt1}>FOR</Text>
             </View>
-        </SafeAreaView>
+            <View>
+                <Text style={styles.txt2}>uunu</Text>
+            </View>
+            <View style={styles.icn1}>
+                <MaterialCommunityIcons style={{ fontSize: 30, color: 'white' }} name='broadcast' />
+                <Feather style={{ fontSize: 30, color: 'white' }} name='search' />
+                {/* <MaterialCommunityIcons style={{ fontSize: 30, }} name='face-man-profile' /> */}
+                <Image style={styles.img1} source={require('../../assets/icon.png')} />
+            </View>
+        </View>
+
     )
 }
 
@@ -20,9 +28,38 @@ const styles = StyleSheet.create({
         backgroundColor: "#ccc",
     },
     view1: {
-
+        height: 100,
+        width: 415,
+        borderWidth: 1,
+        backgroundColor: 'brown',
     },
     txt1: {
-
+        marginTop: 50,
+        marginLeft: 20,
+        fontSize: 25,
+        fontWeight: '600',
+        textTransform: 'capitalize',
+    },
+    txt2: {
+        marginTop: -29,
+        marginLeft: 65,
+        fontSize: 25,
+        fontWeight: '600',
+        textTransform: 'uppercase',
+    },
+    icn1: {
+        marginTop: -32,
+        marginLeft: 260,
+        height: 30,
+        width: 150,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    img1: {
+        width: 30,
+        height: 30,
     }
 });
